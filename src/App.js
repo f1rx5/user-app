c0375fe01544c3c935d39e63dd3153bdacbcfcd9
Firas Sharief <firasshareef30@gmail.com>
2023-01-23 21:04:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import UserInput from "./Components/Users/AddUser";
+const App = () => {
+  return (
+    <div>
+      <UserInput />
+    </div>
+  );
+};
+
+export default App;
